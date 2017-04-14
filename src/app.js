b48961d9188a2724d2271eeccaddfcc0687f15d9
liang <1951828835@qq.com>
2017-04-14 10:28:14 +0800

    初始化目录

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider} from 'react-redux';
+import { creatStore } from 'redux';
+
+// let store = creatStore();
+//
+// render(
+//     <Provider store={store}>
+//
+//     </Provider>,
+//     document.getElementById('root')
+// )
+import './less/common.less';
+// function aa(){
+//     console.log('aa')
+// }
+//
+// function bb(){
+//     console.log('bb')
+// }
+//
+// bb();
+console.log('asdf')
+// alert('asdf')
---
+++ src/maths.js
@@ -0,0 +1,9 @@
+// 这个函数不在任何地方被使用
+export function square(x) {
+    return x * x; //平方
+}
+
+// 这个函数被其他脚本使用
+export function cube(x) {
+    return x * x * x; //立方
+}
