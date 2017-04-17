10f49feb21bffc150a43cd434dcf5a41704e5ce9
liang <1951828835@qq.com>
2017-04-17 18:22:31 +0800

    up root.js

--- src/containers/root.js
+++ src/containers/root.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import smaller from '../assets/img/smaller.png';
 export const Root = () => (
-    <div>Hello React!
+    <div>Hello React！
         <i className="glyphicon glyphicon-plus"></i>
         <div className="bbb"></div>
     </div>
